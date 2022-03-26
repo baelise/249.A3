2eb3836bce248e7eb4166fb8d2a2208e398e7d94
baelise <94066054+baelise@users.noreply.github.com>
2022-03-25 23:15:54 -0400

    Update CSVDataMissing.java

--- CSVDataMissing.java
+++ CSVDataMissing.java
@@ -1,15 +1,21 @@
-
-public class CSVAttributeMissing extends Exception {
+//--------------------------------------------------------------------
+// Elise Proulx (40125538) and Andrei Barbulescu (40208635)
+// COMP 249 - Section S
+// Assignment 3
+// 03/25/2022
+//--------------------------------------------------------------------
+/** CSVDataMissing class. Prints or returns an error message when called*/
+public class CSVDataMissing extends Exception {
 	
-	public CSVAttributeMissing() {
-		super("\n\n***Error 2: Input row cannot be parsed due to missing information");
+	public CSVDataMissing() {
+		super("\n\n***Error 1: Input row cannot be parsed due to missing information");
 	}
 	
-	public CSVAttributeMissing(String fileName) {
-		System.out.println("Error converting file '" + fileName + "' to HTML due to missing attribute.");
+	public CSVDataMissing(String fileName, int lineNumber, String attribute) {
+		System.out.println("**Error converting file '" + fileName + "' to HTML at line " + lineNumber + " due to missing data: " + attribute);
 	}
 	
-	public String getMessage(String fileName) {
-		return "Error converting file '" + fileName + "' to HTML due to missing attribute.";
+	public String getMessage(String fileName, int lineNumber, String[] attribute, int index) {
+		return "**Error, file '" + fileName + "' at line " + lineNumber + " has missing data: " + attribute[index];
 	}
 }
