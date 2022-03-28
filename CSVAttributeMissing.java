a525026e6a87e0912afc513202e08fb94c68723e
baelise <94066054+baelise@users.noreply.github.com>
2022-03-28 11:55:30 -0400

    Update CSVAttributeMissing.java

--- CSVAttributeMissing.java
+++ CSVAttributeMissing.java
@@ -6,15 +6,15 @@
 //--------------------------------------------------------------------
 /** CSVAttributeMissing class. Prints or returns an error message when called*/
 public class CSVAttributeMissing extends Exception {
-	
+	/**Default Constructor*/
 	public CSVAttributeMissing() {
 		super("\n\n***Error 2: Input row cannot be parsed due to missing information");
 	}
-	
+	/**Constructor will print an error message to console*/
 	public CSVAttributeMissing(String fileName) {
 		System.out.println("**Error converting file '" + fileName + "' to HTML due to missing attribute.");
 	}
-	
+	/**Method will return an error message*/
 	public String getMessage(String fileName) {
 		return "**Error converting file '" + fileName + "' to HTML due to missing attribute.";
 	}
